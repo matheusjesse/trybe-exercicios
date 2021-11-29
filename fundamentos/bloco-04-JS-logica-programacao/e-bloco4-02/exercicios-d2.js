0053f4cc0746f5e6b82e18d99d313eb7eb1089dd
Matheus <matheus_jesse@hotmail.com>
2021-11-29 18:50:07 -0300

    exercicio 3

--- fundamentos/bloco-04-JS-logica-programacao/e-bloco4-02/exercicios-d2.js
+++ fundamentos/bloco-04-JS-logica-programacao/e-bloco4-02/exercicios-d2.js
@@ -1,6 +1,10 @@
+//Exercicio 1
+//Exercicio 2
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 console.log(numbers);
+
+
 
 let somaArrays = 0;
 
@@ -8,4 +12,9 @@ for(let i = 0; i < numbers.length; i++){
     somaArrays = somaArrays + numbers[i];
 }
 
-return console.log(somaArrays);
+console.log(somaArrays);
+
+
+let mediaAritimetica = somaArrays / numbers.length;
+
+console.log("Média aritimética: " + mediaAritimetica);
