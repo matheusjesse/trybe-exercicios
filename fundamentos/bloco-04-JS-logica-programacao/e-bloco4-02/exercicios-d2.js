a304ad833f8fdb575e0dd8f29e23ff3baa4e4ca5
Matheus <matheus_jesse@hotmail.com>
2021-11-29 18:45:32 -0300

    Exercicio 02

--- fundamentos/bloco-04-JS-logica-programacao/e-bloco4-02/exercicios-d2.js
+++ fundamentos/bloco-04-JS-logica-programacao/e-bloco4-02/exercicios-d2.js
@@ -1,3 +1,11 @@
-let numbers = [5, 9,3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 console.log(numbers);
+
+let somaArrays = 0;
+
+for(let i = 0; i < numbers.length; i++){
+    somaArrays = somaArrays + numbers[i];
+}
+
+return console.log(somaArrays);
