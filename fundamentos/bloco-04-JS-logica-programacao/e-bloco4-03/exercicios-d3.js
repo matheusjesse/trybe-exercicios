fa3ae9c3c581388fac2c9ea8771e5885f5facde4
Matheus <matheus_jesse@hotmail.com>
2021-11-30 18:51:43 -0300

    bloco4-dia3-exercicio02

--- fundamentos/bloco-04-JS-logica-programacao/e-bloco4-03/exercicios-d3.js
+++ fundamentos/bloco-04-JS-logica-programacao/e-bloco4-03/exercicios-d3.js
@@ -1,6 +1,6 @@
 //Exercício 1
 
-let n = 10;
+let n = 5;
 
 let asterisco = '';
 
@@ -11,4 +11,13 @@ for(let x = 0; x < n; x++){
 
 for(let i = 0; i < n; i++){
     console.log(asterisco);
+}
+
+//Exercício 2
+
+let asterisco2 = '';
+
+for(let y = 0; y < n; y++){
+    asterisco2 += '*';
+    console.log(asterisco2);
 }
